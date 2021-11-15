1b0472d08eb30eb730d0a5afb7f37b419cff93fd
mono6 <rudcjs42@naver.com>
2021-11-15 22:52:19 +0900

    Stack, queue, hash, heap

--- inflearn/session5/5-6.py
+++ inflearn/session5/5-6.py
@@ -26,15 +26,22 @@
 # ▣ 출력예제 1
 # blue
 
+# n = int(input())
+# p = dict()
+# for i in range(n):
+#     word = input()
+#     p[word] = 1
+# for i in range(n-1):
+#     word = input()
+#     p[word] = 0
+# for key, val in p.items():
+#     if val == 1:
+#         print(key)
+#         break
+
+# solution2
 n = int(input())
-p = dict()
-for i in range(n):
-    word = input()
-    p[word] = 1
-for i in range(n-1):
-    word = input()
-    p[word] = 0
-for key, val in p.items():
-    if val == 1:
-        print(key)
-        break
+words = [input() for _ in range(n)]
+finds = [input() for _ in range(n-1)]
+
+print(*(set(words) - set(finds)))
